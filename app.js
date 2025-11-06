dfb68e87eed9e884944d4484e4cb5d8097fd0da3
Rin <82919982+Rin-chan@users.noreply.github.com>
2025-11-07 04:58:34 +0800

    fix digital certificate

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const multer = require('multer');
 const upload = multer();
 
 const mysql = require('mysql2');
+const fs = require('fs');
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized:true }
+    port: 3306,
+    ssl: {
+        ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem"),
+        rejectUnauthorized: true
+    }
 })
 
 // Startup Database (and create tables)
