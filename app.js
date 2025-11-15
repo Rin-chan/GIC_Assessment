c356cb762d547049146dc1f9a5e417fa7fd964f9
Rin <82919982+Rin-chan@users.noreply.github.com>
2025-11-15 20:25:43 +0800

    Added backend validation

--- app.js
+++ app.js
@@ -55,6 +55,53 @@ function makeid() {
     return result;
 }
 
+const nameError = "Length of name must be between 6 and 10 characters.";
+const descriptionError = "Length of description must be less than 256 characters.";
+const logoError = "Image should be less than 2mb.";
+const emailError = "Input a valid email.";
+const phoneError = "Input a valid phone number.";
+const genderError = "Click the gender button again.";
+const cafeError = "Select a cafe again.";
+
+function nameCheck(name) {
+    return (name.length < 6 || name.length > 10) ? false : true;
+}
+
+function descriptionCheck(description) {
+    return (description.length > 256) ? false : true;
+}
+
+function logoCheck(logo) {
+    const logoString = logo.substring(logo.indexOf(',') + 1);
+    const mb = Math.ceil( ( ( (logoString.length * 6) / 8) / 1000) / 1000 );
+
+    return (mb < 2) ? true : false;
+}
+
+function emailCheck(email) {
+    const emailRegex = "^(?!.*\.{2})(?!\.)[a-z0-9_.'-]*[a-z0-9_'-]@(?!_)(?:[a-z0-9_'-]+\.)+[a-z0-9_'-]{2,}$";
+    return (emailRegex.test(email)) ? true : false;
+}
+
+function phoneCheck(phone) {
+    const phoneRegex = "^[8-9]{1}[0-9]{7}$";
+    return (phoneRegex.test(email)) ? true : false;
+}
+
+function genderCheck(gender) {
+    return (gender == "Female" || gender == "Male") ? true : false;
+}
+
+function cafeCheck(cafe) {
+    let cafeList = [];
+    connection.query("SELECT DISTINCT Cafe.name FROM Cafe;", (err, results, fields) => {
+        if (err) return res.status(400).json(`An error has occured: ${err}`);
+        cafeList = results;
+    });
+
+    return (cafeList.indexOf(cafe) == -1) ? false : true;
+}
+
 app.use(express.static(path.join(__dirname, '/gic_assessment/build')));
 
 // Create a GET endpoint /cafes?location=<location>
@@ -65,7 +112,7 @@ app.get('/cafes', (req, res) => {
             GROUP BY Cafe.name
             ORDER BY count(Employee.cafe_name) DESC
             ;`, (err, results, fields) => {
-            if (err) throw res.status(400).json(`An error has occured: ${err}`);
+            if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
             res.json(results);
         });
     } else {
@@ -75,7 +122,7 @@ app.get('/cafes', (req, res) => {
             GROUP BY Cafe.name
             ORDER BY count(Employee.cafe_name) DESC
             ;`, [req.query.location], (err, results, fields) => {
-            if (err) throw res.status(400).json(`An error has occured: ${err}`);
+            if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
             res.json(results);
         });
     }
@@ -87,7 +134,7 @@ app.get('/employees', (req, res) => {
         connection.query(`SELECT id, name, email_address, phone_number, DATEDIFF(CURDATE(), start_date) as days_worked, cafe_name, gender FROM Employee
             ORDER BY DATEDIFF(CURDATE(), start_date) DESC
             ;`, (err, results, fields) => {
-            if (err) throw res.status(400).json(`An error has occured: ${err}`);
+            if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
             res.json(results);
         });
     } else {
@@ -95,7 +142,7 @@ app.get('/employees', (req, res) => {
             WHERE cafe_name = ?
             ORDER BY DATEDIFF(CURDATE(), start_date) DESC
             ;`, [req.query.cafe], (err, results, fields) => {
-            if (err) throw res.status(400).json(`An error has occured: ${err}`);
+            if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
             res.json(results);
         });
     }
@@ -103,45 +150,76 @@ app.get('/employees', (req, res) => {
 
 // Create a POST endpoint /cafes
 app.post('/cafes', upload.none(), (req,res) => {
+    if (!nameCheck(req.body.name)) return res.status(400).json(nameError);
+
+    if (!descriptionCheck(req.body.description)) return res.status(400).json(descriptionError);
+
+    if (!logoCheck(req.body.logo)) return res.status(400).json(logoError);
+
     connection.query(`INSERT INTO CAFE (name, description, logo, location, id) VALUES (?, ?, ? ,?, UUID());`
         , [req.body.name, req.body.description, req.body.logo, req.body.location]
         , (err, results, fields) => {
-        if (err) throw res.status(400).json(`An error has occured: ${err}`);
+        if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
     });
     res.status(200).json({ message: 'Success' });
 });
 
 // Create a POST endpoint /employees
 app.post('/employees', upload.none(), (req,res) => {
-    console.log(req.body);
+    if (!nameCheck(req.body.name)) return res.status(400).json(nameError);
+
+    if (!emailCheck(req.body.email)) return res.status(400).json(emailError);
+
+    if (!phoneCheck(req.body.phone)) return res.status(400).json(phoneError);
+
+    if (!genderCheck(req.body.gender)) return res.status(400).json(genderError);
+
+    if (!cafeCheck(req.body.cafe)) return res.status(400).json(cafeError);
+    
     connection.query(`INSERT INTO Employee (id, name, email_address, phone_number, gender, start_date, cafe_name) VALUES (?, ?, ?, ?, ?, ?, ?);`
     , [`UI${makeid()}`, req.body.name, req.body.email, req.body.phone, req.body.gender, req.body.start_date, req.body.cafe]
     , (err, results, fields) => {
-        if (err) throw res.status(400).json(`An error has occured: ${err}`);
+        if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
     });
     res.status(200).json({ message: 'Success' });
 });
 
 // Create a PUT endpoint /cafes
 app.put('/cafes/:id', upload.none(), (req, res) => {
+    if (!nameCheck(req.body.name)) return res.status(400).json(nameError);
+
+    if (!descriptionCheck(req.body.description)) return res.status(400).json(descriptionError);
+
+    if (!logoCheck(req.body.logo)) return res.status(400).json(logoError);
+
     connection.query(`UPDATE Cafe
         SET name = ?, description = ?, logo = ?, location = ?
         WHERE id = ?;`
         , [req.body.name, req.body.description, req.body.logo, req.body.location, req.params.id]
         , (err, results, fields) => {
-        if (err) throw res.status(400).json(`An error has occured: ${err}`);
+        if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
     });
     res.status(200).json({ message: 'Success' });
 });
 
 // Create a PUT endpoint /employees
 app.put('/employees/:id', upload.none(), (req, res) => {
+    if (!nameCheck(req.body.name)) return res.status(400).json(nameError);
+
+    if (!emailCheck(req.body.email)) return res.status(400).json(emailError);
+
+    if (!phoneCheck(req.body.phone)) return res.status(400).json(phoneError);
+
+    if (!genderCheck(req.body.gender)) return res.status(400).json(genderError);
+
+    if (!cafeCheck(req.body.cafe)) return res.status(400).json(cafeError);
+
     connection.query(`UPDATE Employee
         SET name = ?, email_address = ?, phone_number = ?, gender = ?, cafe_name = ?
         WHERE id = ?;`
         , [req.body.name, req.body.email, req.body.phone, req.body.gender, req.body.cafe, req.params.id]
         , (err, results, fields) => {
-        if (err) throw res.status(400).json(`An error has occured: ${err}`);
+        if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
     });
     res.status(200).json({ message: 'Success' });
 });
@@ -152,7 +230,7 @@ app.delete('/cafes/:name', (req, res) => {
         WHERE name = ?;`
         , [req.params.name]
         , (err, results, fields) => {
-        if (err) throw res.status(400).json(`An error has occured: ${err}`);
+        if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
     });
     res.status(200).json({ message: 'Success' });
 })
@@ -163,7 +241,7 @@ app.delete('/employees/:id', (req, res) => {
         WHERE id = ?;`
         , [req.params.id]
         , (err, results, fields) => {
-        if (err) throw res.status(400).json(`An error has occured: ${err}`);
+        if (err) return res.status(400).json({ message: `An error has occured: ${err}`});
     });
     res.status(200).json({ message: 'Success' });
 })
